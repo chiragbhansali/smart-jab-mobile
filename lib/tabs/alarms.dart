38fc7806d24b607d6de4943b41b92ec5a0ffb5f5
Stryker998 <adityabhatt998@gmail.com>
2021-06-01 00:07:49 +0530

    Added Vibrate

--- lib/tabs/alarms.dart
+++ lib/tabs/alarms.dart
@@ -14,7 +14,6 @@ class AlarmsTab extends StatefulWidget {
 // add this line wherever platform required
 // final int result = await platform.invokeMethod(<NAME>)
 
-
 class _AlarmsTabState extends State<AlarmsTab> {
   bool isLoading = true;
   List<Alarm> alarms;
@@ -117,7 +116,7 @@ class AlarmCard extends StatefulWidget {
 
 class _AlarmCardState extends State<AlarmCard> {
   Alarm alarm;
-
+  bool isVibrateChecked = false;
   bool toBool(String v) {
     return v == "true";
   }
@@ -148,83 +147,109 @@ class _AlarmCardState extends State<AlarmCard> {
         // }
       },
       child: Container(
-        //height: 170,
-        width: MediaQuery.of(context).size.width - 54,
-        padding: EdgeInsets.only(left: 15, top: 20, bottom: 40, right: 15),
-        margin: EdgeInsets.only(left: 22, right: 22, top: 10, bottom: 10),
-        decoration: BoxDecoration(
-            color: Color(0xffF5F7FA),
-            borderRadius: BorderRadius.circular(8),
-            boxShadow: [
-              BoxShadow(
-                  color: Color(0xff3E4C59).withOpacity(0.2),
-                  blurRadius: 6,
-                  spreadRadius: 0,
-                  offset: Offset(0, 2))
-            ]),
-        child:Column(
-          children: [
-            Row(
-              children: [
-                Container(
-                  constraints: BoxConstraints(
-                      maxWidth: MediaQuery.of(context).size.width - 158),
-                  child: Text(
-                    alarm.pincode ?? alarm.districtName,
-                    style: TextStyle(
-                        fontSize: 18,
-                        color: Color(0xff323F4B),
-                        fontWeight: FontWeight.w600),
-                  ),
-                ),
-                Expanded(child: Container()),
-                Switch(
-                    value: toBool(alarm.isOn),
-                    onChanged: (v) async {
-                      await DatabaseProvider.db.editAlarmOnState(alarm.id, v);
-                      setState(() {
-                        alarm.isOn = v.toString();
-                      });
-                    }),
-              ],
-            ),
-            SizedBox(height: 31, width: MediaQuery.of(context).size.width,),
-            Row(
-              children: [
-                GestureDetector(
-                  onTap: () async {
-                    const platform = const MethodChannel(
-                      'com.arnav.smartjab/flutter',
-                    );
-                    var result =
-                         await platform.invokeMethod("chooseRingtone");
-                  },
-                    child: Container(
+          //height: 170,
+          width: MediaQuery.of(context).size.width - 54,
+          padding: EdgeInsets.only(left: 15, top: 20, bottom: 20, right: 15),
+          margin: EdgeInsets.only(left: 22, right: 22, top: 10, bottom: 10),
+          decoration: BoxDecoration(
+              color: Color(0xffF5F7FA),
+              borderRadius: BorderRadius.circular(8),
+              boxShadow: [
+                BoxShadow(
+                    color: Color(0xff3E4C59).withOpacity(0.2),
+                    blurRadius: 6,
+                    spreadRadius: 0,
+                    offset: Offset(0, 2))
+              ]),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
                     constraints: BoxConstraints(
                         maxWidth: MediaQuery.of(context).size.width - 158),
-                      child: Row(
-                        children: [
-                          Icon(
-                            Icons.notifications_active_outlined,
-                            color: Color.fromRGBO(62, 76, 89, 1),
-                            size: 24,
-                          ),
-                          SizedBox(width: 12),
-                          Text("Default(Oxygen)",
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: Color.fromRGBO(62, 76, 89, 1),
-                                fontWeight: FontWeight.w500,
-                              )),
-                        ],
+                    child: Text(
+                      alarm.pincode ?? alarm.districtName,
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: Color(0xff323F4B),
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  Expanded(child: Container()),
+                  Switch(
+                      value: toBool(alarm.isOn),
+                      onChanged: (v) async {
+                        await DatabaseProvider.db.editAlarmOnState(alarm.id, v);
+                        setState(() {
+                          alarm.isOn = v.toString();
+                        });
+                      }),
+                ],
+              ),
+              SizedBox(height: 20, width: MediaQuery.of(context).size.width),
+              Row(
+                children: [
+                  GestureDetector(
+                      onTap: () async {
+                        const platform = const MethodChannel(
+                          'com.arnav.smartjab/flutter',
+                        );
+                        var result =
+                            await platform.invokeMethod("chooseRingtone");
+                      },
+                      child: Container(
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.notifications_active_outlined,
+                              color: Color.fromRGBO(62, 76, 89, 1),
+                              size: 24,
+                            ),
+                            SizedBox(width: 12),
+                            Text("Default(Oxygen)",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: Color.fromRGBO(62, 76, 89, 1),
+                                  fontWeight: FontWeight.w500,
+                                )),
+                          ],
+                        ),
+                      )),
+                  Expanded(child: Container(), flex: 1),
+                  Checkbox(
+                    value: isVibrateChecked,
+                    onChanged: (value) {
+                      setState(() {
+                        isVibrateChecked = value;
+                        print(isVibrateChecked);
+                      });
+                    },
+                  ),
+                  Expanded(child: Container(), flex: 0),
+                  GestureDetector(
+                    onTap: () async {
+                      setState(() {
+                        isVibrateChecked = !isVibrateChecked;
+                        print(isVibrateChecked);
+                      });
+                    },
+                    child: Text(
+                      "Vibrate",
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Color(0xff3E4C59),
+                        fontWeight: FontWeight.w500,
                       ),
-                    )
-                ),
-              ],
-            ),
-          ],
-        )
-      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  )
+                ],
+              ),
+            ],
+          )),
     );
   }
 }
