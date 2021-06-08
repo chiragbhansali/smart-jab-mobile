a9d927b31474d18727537b0a01153f76109375f9
Stryker998 <adityabhatt998@gmail.com>
2021-06-08 13:23:34 +0530

    added small details in hospital screen

--- lib/views/available/hospital.dart
+++ lib/views/available/hospital.dart
@@ -1,16 +1,43 @@
+import 'dart:convert';
+import "package:http/http.dart" as http;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HospitalScreen extends StatefulWidget {
+  final String centerId;
   final String name;
   final String address;
-  const HospitalScreen({this.name, this.address});
+  final dynamic age;
+  final String vaccine;
+  final String selectedDate;
+  const HospitalScreen(
+      {this.centerId,
+      this.name,
+      this.address,
+      this.age,
+      this.vaccine,
+      this.selectedDate});
 
   @override
   _HospitalScreenState createState() => _HospitalScreenState();
 }
 
 class _HospitalScreenState extends State<HospitalScreen> {
+  var apiData;
+  void getCenterData() async {
+    print(widget.selectedDate);
+    var data = await http.get(Uri.parse(
+        "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByCenter?center_id=${widget.centerId}&date=${widget.selectedDate}"));
+    var body = jsonDecode(data.body);
+    apiData = body;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +52,16 @@ class _HospitalScreenState extends State<HospitalScreen> {
                 color: Color(0xff323F4B),
                 fontWeight: FontWeight.w500,
                 fontSize: 18)),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Color(0xff0A6CFF),
+        child: Center(
+          child: Icon(
+            Icons.directions_outlined,
+            color: Colors.white,
+          ),
+        ),
       ),
       body: Container(
         padding: EdgeInsets.only(
@@ -56,6 +93,74 @@ class _HospitalScreenState extends State<HospitalScreen> {
                     color: Color(0xff3E4C59),
                     fontSize: 16,
                     fontWeight: FontWeight.w500)),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.person_outline_outlined,
+                  color: Theme.of(context).primaryColor,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Wrap(
+                  spacing: 8,
+                  children: widget.age
+                      .map<Widget>(
+                        (a) => Container(
+                          child: Text("$a+",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color(0xff3E4C59))),
+                        ),
+                      )
+                      .toList(),
+                ),
+                Expanded(
+                  child: Container(),
+                ),
+                Icon(
+                  Icons.medication_outlined,
+                  color: Theme.of(context).primaryColor,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  "${widget.vaccine}",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xff3E4C59)),
+                ),
+                Expanded(
+                  child: Container(),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              "Slot opening history",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                  color: Color(0xff323F4B)),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Text(
+              "Slot Availability",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                  color: Color(0xff323F4B)),
+            ),
           ],
         ),
       ),
